cc7442750d083fd1abcad4ef4979d4324ba00c02
lexaz777 <vh1@mail.ru>
2019-06-22 13:13:14 +0300

    goip gateway class amend

--- gitbook2/src/main/java/entity/GoipGateway.java
+++ gitbook2/src/main/java/entity/GoipGateway.java
@@ -1,4 +1,48 @@
 package entity;
 
 public class GoipGateway {
+    private int id;
+    private String title;
+    private int port;
+    private int ip;
+
+    public GoipGateway(int id, String title, int port, int ip) {
+        this.id = id;
+        this.title = title;
+        this.port = port;
+        this.ip = ip;
+    }
+
+    public int getId() {
+        return id;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getIp() {
+        return ip;
+    }
+
+    public void setIp(int ip) {
+        this.ip = ip;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
